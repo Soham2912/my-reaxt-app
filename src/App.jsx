295839225646ed4a13e7d02e200cbb7ebc63a2d9
Soham2912 <sohamlahoti@gmail.com>
2024-06-09 18:54:26 +0530

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import Header from "./Header"
-import Footer from "./Footer.jsx"
+
 function App() {
   return(
-    <>
-    <Header></Header>
-    <Footer></Footer>
-    </>
+    
+  
   );
 }
 
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+
+return(
+    <div className="card">
+        <img></img>
+        <h2>Void</h2>
+        <p></p>
+    </div>
+);
+
+}
+
+export default Card;
